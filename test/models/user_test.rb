34f089a51412c4734e6356c196262095f3e965d7
KyoheiSasaki <danibusters.main@gmail.com>
2018-09-28 12:39:25 +0900

    add test name-should-be-present i test/models/user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,11 +6,15 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-	  @user = User.new(name: "exampleuser", email: "user@example.com") 
+	@user = User.new(name: "exampleuser", email: "user@example.com") 
   end
 
   test "should be valid" do
 	  assert @user.valid?
   end
+
+  test "name should be present" do
+	  @user = "    "
+	  assert_not @user.valid?
 
 end
